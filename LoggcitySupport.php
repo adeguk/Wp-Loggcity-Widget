96602d5d03ed1cbd204001ab20968193ada698fa
Adewale Adegoroye <adeguk@gmail.com>
2021-02-13 16:24:54 +0000

    Remove WP embed script and hide admin notice
    Hide admin notice
    Remove WordPress Version Number
    Disable the emoji's
    Remove WP embed script

--- LoggcitySupport.php
+++ LoggcitySupport.php
@@ -3,7 +3,7 @@
 Plugin Name: Loggcity Support Widget
 Plugin URI:	 https://.loggcity.africa
 Description: Loggcity Support info available via dashboard widget code
-Version: 1.0
+Version: 1.5
 Author: Adewale Adegoroye
 Author URI: https://.waleadegoroye.com
 Company: Loggcity
@@ -228,3 +228,64 @@ Function get_site_webmail(){
 
 	echo "https://webmail.$domain";
 }
+
+/** IMPROVE PERFORMANCE */
+// Defer parsing of js
+function defer_parsing_of_js( $url ) {
+    if ( is_user_logged_in() ) return $url; //don't break WP Admin
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.js' ) ) return $url;
+    return str_replace( ' src', ' defer src', $url );
+}
+add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
+
+// Hide admin notice
+function hide_update_msg_non_admins(){
+    echo '<style>#setting-error-tgmpa>.updated settings-error notice is-dismissible, .update-nag, .updated { display: none; }</style>';
+}
+add_action( 'admin_head', 'hide_update_msg_non_admins');
+
+// Remove WordPress Version Number
+function wpb_remove_version() {
+    return '';
+}
+add_filter('the_generator', 'wpb_remove_version');
+// Disable XML-RPC in WordPress
+add_filter('xmlrpc_enabled', '__return_false');
+
+/**
+ * Disable the emoji's
+ */
+function disable_emojis() {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+
+add_action( 'init', 'disable_emojis' );
+// The Emojis now come also with a DNS prefetch, which can be disabled by adding this line:
+add_filter( 'emoji_svg_url', '__return_false' );
+
+// Remove WP embed script
+function speed_stop_loading_wp_embed() {
+    if (!is_admin()) {
+        wp_deregister_script('wp-embed');
+    }
+}
+// Remove query strings with code#
+// The first way to remove query strings is you can put the following code into your functions.php file:
+
+function _remove_script_version( $src ) {
+    $parts = explode( '?ver', $src );
+    return $parts[0];
+}
+
+add_filter( 'script_loader_src', '_remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', '_remove_script_version', 15 );
+add_action('init', 'speed_stop_loading_wp_embed');
+// disable used plugin’s stylesheet
